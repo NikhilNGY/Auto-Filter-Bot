50891568f6a06658e8e394720198a55a98b88f4f
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-13 16:59:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '') #bot token from botfather
 if len(BOT_TOKEN) == 0:
     print('Error - BOT_TOKEN is missing, exiting now')
     exit()
-PORT = int(environ.get('PORT', '80')) #don't change anything 
+PORT = int(environ.get('PORT', '8080')) #don't change anything 
 
 # Bot pics
 PICS = (environ.get('PICS', 'https://envs.sh/t3L.jpg')).split()
